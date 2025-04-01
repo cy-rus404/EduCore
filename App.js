5a8f994c2bd2b8eeae6b31084cda65946ca40430
cy-rus404 <sduisaac@yahoo.com>
2025-04-01 11:04:05 +0000

    commit

--- App.js
+++ App.js
@@ -3,10 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './Screens/SplashScreen';
 import HomeScreen from './Screens/HomeScreen';
-import LoginScreen from './Screens/LoginScreen';
-import AdminDashboard from './Screens/AdminDashboard';
-import StudentCategoriesScreen from './Screens/StudentCategoriesScreen';
-import StudentListScreen from './Screens/StudentListScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,10 +12,6 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
-        <Stack.Screen name="StudentCategories" component={StudentCategoriesScreen} />
-        <Stack.Screen name="StudentList" component={StudentListScreen} />
 
       </Stack.Navigator>
     </NavigationContainer>
